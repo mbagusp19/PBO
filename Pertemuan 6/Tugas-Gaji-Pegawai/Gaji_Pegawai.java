d0c96ec466d6d891c5902778a6b754ed11d12912
mbagusp19 <71370641+mbagusp19@users.noreply.github.com>
2020-11-02 14:35:49 +0700

    Update Gaji_Pegawai.java

--- Pertemuan 6/Tugas-Gaji-Pegawai/Gaji_Pegawai.java
+++ Pertemuan 6/Tugas-Gaji-Pegawai/Gaji_Pegawai.java
@@ -2,30 +2,27 @@ package com.pboreg;
 
 import java.util.*;
 
-class Orang{
-    private int _umur;
-    private String _nama;
-    private String golDarah;
-    
-    public void setUmur(int umur){ this._umur = umur; }
-    public void setNama(String nama){ this._nama = nama; }
+class Data{
+    private String nama, golDarah;
+    private int umur;
+
+    public void setUmur(int umur){ this.umur = umur; }
+    public void setNama(String nama){ this.nama = nama; }
     public void setGolDarah(String golDarah){ this.golDarah = golDarah; }
-    public int getUmur (){ return this._umur; }
+    public int getUmur (){ return this.umur; }
     public String getGolDarah (){ return golDarah; }
-    public String getNama (){ return this._nama   ; }
+    public String getNama (){ return this.nama   ; }
 
 }
 
-class Pegawai extends Orang {
-    String nip;
-    String jabatan;
-    int golongan;
-    int jamKerja;
+class Karyawan extends Data {
+    String nip, jabatan;
+    int gol, jam;
 
-    public int getGolongan() { return golongan; }
-    public void setGolongan(int golongan) { this.golongan = golongan; }
-    public int getJamKerja() { return jamKerja; }
-    public void setJamKerja(int jamKerja) { this.jamKerja = jamKerja; }
+    public int getGolongan() { return gol; }
+    public void setGolongan(int gol) { this.gol = gol; }
+    public int getJam() { return jam; }
+    public void setJam(int jam) { this.jam = jam; }
     public String getNip() { return nip; }
     public void setNip(String nip) { this.nip = nip; }
     public String getJabatan() { return jabatan; }
@@ -33,102 +30,94 @@ class Pegawai extends Orang {
     public static double persenPajak(double a, int b){ return a / b; }
 
     void gaji() {
-        int gajipokok = 0;
-        int tunjangan = 0;
-        int gajilembur;
-        int totalgaji;
-        int lembur;
-        double gajipajak;
+        int gapok = 0, tunjangan = 0, glembur, totalgaji, lembur;
+        double gpajak;
 
-        if (golongan == 1) {
-            gajipokok = 1486500;
+        if (gol == 1) {
+            gapok = 1486500;
             tunjangan = 300000;
         }
-        if (golongan == 2) {
-            gajipokok = 1926000;
+        if (gol == 2) {
+            gapok = 1926000;
             tunjangan = 300000;
         }
-        if (golongan == 3) {
-            gajipokok = 2456700;
+        if (gol == 3) {
+            gapok = 2456700;
             tunjangan = 350000;
         }
-        if (golongan == 4) {
-            gajipokok = 2899500;
+        if (gol == 4) {
+            gapok = 2899500;
             tunjangan = 400000;
         }
-        if (jamKerja <= 172) {
+        if (jam <= 172) {
             lembur = 0;
-            System.out.println("\t MAAF ANDA TIDAK DAPAT UANG LEMBUR");
+            System.out.println("\t MAAF ANDA TIDAK MENDAPAT UANG LEMBUR");
             System.out.println(" ");
         }
         else {
-            lembur = jamKerja - 173;
+            lembur = jam - 173;
         }
-        gajilembur = lembur * 20000;
-        totalgaji = gajipokok + tunjangan + gajilembur;
+        glembur = lembur * 20000;
+        totalgaji = gapok + tunjangan + glembur;
         double Pajak = persenPajak(0.5, 100) * totalgaji;
 
-        System.out.println("Gaji Pokok Anda  = Rp. " + gajipokok);
-        System.out.println("Tunjangan Anda   = Rp. " + tunjangan);
-        System.out.println("Gaji Lembur Anda = Rp. " + gajilembur);
-        gajipajak = totalgaji - Pajak;
+        System.out.println("Gaji Pokok  = Rp. " + gapok);
+        System.out.println("Tunjangan   = Rp. " + tunjangan);
+        System.out.println("Gaji Lembur = Rp. " + glembur);
+        System.out.println("Pajak       = Rp. " + Pajak);
+        gpajak = totalgaji - Pajak;
         System.out.println(" ");
-        System.out.println("\t TOTAL GAJI ANDA ADALAH = RP. " + gajipajak);
+        System.out.println("\t TOTAL GAJI ANDA ADALAH = Rp. " + gpajak);
         System.out.println(" ");
 
     }
 }
 
-public class Gaji_Pegawai {
+public class Hitung_Gaji {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Pegawai peg = new Pegawai();
+        Karyawan kar = new Karyawan();
 
-        int jamKerja;
-        int umur;
-        int golongan;
-        String nama;
-        String golonganDarah;
-        String nip;
-        String jabatan;
+        int jam, umur, gol;
+        String nama, golDarah, nip, jabatan;
 
-        System.out.println("\t SILAHKAN MASUKKAN BIODATA ANDA");
+        System.out.println("\t MASUKKAN BIODATA ANDA");
         System.out.print("Nama : ");
         nama = input.nextLine();
         System.out.print("NIP : ");
         nip = input.nextLine();
         System.out.print("Golongan Darah : ");
-        golonganDarah = input.nextLine();
+        golDarah = input.nextLine();
         System.out.print("Jabatan : ");
         jabatan = input.nextLine();
         System.out.print("Umur : ");
         umur = input.nextInt();
         System.out.print("Jam kerja : ");
-        jamKerja = input.nextInt();
+        jam = input.nextInt();
         System.out.print("Golongan Pekerja : ");
-        golongan = input.nextInt();
+        gol = input.nextInt();
         System.out.println(" ");
         System.out.println("\t BIODATA ");
 
-        peg.setNama(nama);
-        peg.setNip(nip);
-        peg.setUmur(umur);
-        peg.setGolDarah(golonganDarah);
-        peg.setJabatan(jabatan);
-        peg.setJamKerja(jamKerja);
-        peg.setGolongan(golongan);
+        kar.setNama(nama);
+        kar.setNip(nip);
+        kar.setUmur(umur);
+        kar.setGolDarah(golDarah);
+        kar.setJabatan(jabatan);
+        kar.setJam(jam);
+        kar.setGolongan(gol);
 
-        System.out.println("Nama : " + peg.getNama());
-        System.out.println("NIP : " + peg.getNip());
-        System.out.println("Golongan Darah : " + peg.getGolDarah());
-        System.out.println("Jabatan : " + peg.getJabatan());
-        System.out.println("Umur : " + peg.getUmur());
-        System.out.println("Jam Kerja : " + peg.getJamKerja());
-        System.out.println("Golongan Pekerja: " + peg.getGolongan());
+        System.out.println("Nama : " + kar.getNama());
+        System.out.println("NIP : " + kar.getNip());
+        System.out.println("Golongan Darah : " + kar.getGolDarah());
+        System.out.println("Jabatan : " + kar.getJabatan());
+        System.out.println("Umur : " + kar.getUmur());
+        System.out.println("Jam Kerja : " + kar.getJam());
+        System.out.println("Golongan Pekerja : " + kar.getGolongan());
         System.out.println(" ");
-        System.out.println("\t GOLONGAN KE- " + golongan);
-        peg.gaji();
+        System.out.println("\t GOLONGAN KE- " + gol);
+        kar.gaji();
 
     }
 
